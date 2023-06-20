afe8cf8937f7bdeb730de631566e88e6bfbeffde
Евгений Зимин <evgeney.zimin@graphics.cs.msu.ru>
2023-06-20 16:51:14 +0300

    Shader backup

---
+++ Assets/Scripts/Animations/PostProcessing.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+
+
+[Serializable]
+[PostProcess(typeof(PostProcessOutlineRenderer), PostProcessEvent.AfterStack, "Outline")]
+public sealed class PostProcessOutline : PostProcessEffectSettings
+{
+    public FloatParameter thickness = new FloatParameter { value = 1f };
+    public FloatParameter depthMin = new FloatParameter { value = 0f };
+    public FloatParameter depthMax = new FloatParameter { value = 1f };
+}
+
+
+public sealed class PostProcessOutlineRenderer : PostProcessEffectRenderer<PostProcessOutline>
+{
+    public override void Render(PostProcessRenderContext context)
+    {
+        var sheet = context.propertySheets.Get(Shader.Find("Hidden/Roystan/Outline Post Process"));
+        sheet.properties.SetFloat("_Thickness", settings.thickness);
+        sheet.properties.SetFloat("_MinDepth", settings.depthMin);
+        sheet.properties.SetFloat("_MaxDepth", settings.depthMax);
+        
+        context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+    }
+}
